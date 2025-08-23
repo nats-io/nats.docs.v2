1d394d2e083418d82908fdd9f8fe04e19a0a3d7f
Byron Ruth <byron@synadia.com>
2025-08-23 08:57:45 -0400

    Add aliases and reloadable components
    This is used in the generated markdown for the server config.
    
    Signed-off-by: Byron Ruth <byron@synadia.com>

---
+++ src/components/Reloadable.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Icon } from "@iconify/react";
+
+export default function Reloadable() {
+  return (
+    <>
+      <Icon icon="mage:reload" inline /> Hot Reloadable
+    </>
+  );
+}
---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,11 @@
+import MDXComponents from "@theme-original/MDXComponents";
+import { Icon } from "@iconify/react";
+import Reloadable from "@site/src/components/Reloadable";
+import Aliases from "@site/src/components/Aliases";
+
+export default {
+  ...MDXComponents,
+  Icon,
+  Reloadable,
+  Aliases,
+};
