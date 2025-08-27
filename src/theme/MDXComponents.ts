7d4db550bbcaa9a71e458e7db0decfba32fc5f0d
Byron Ruth <byron@synadia.com>
2025-08-27 08:37:57 -0400

    Remove lingering iconify dependency
    Signed-off-by: Byron Ruth <byron@synadia.com>

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,12 +1,10 @@
 import MDXComponents from "@theme-original/MDXComponents";
-import { Icon } from "@iconify/react";
 import Reloadable from "@site/src/components/Reloadable";
 import Aliases from "@site/src/components/Aliases";
 import JSONSchema from "../components/JSONSchema";
 
 export default {
   ...MDXComponents,
-  Icon,
   Reloadable,
   Aliases,
   JSONSchema,
