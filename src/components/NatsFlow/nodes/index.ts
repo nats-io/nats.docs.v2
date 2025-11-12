745ce445b3a9f3a0cdaf006534e3949be7c569d3
Tomasz Pietrek <tomasz@synadia.com>
2025-11-12 12:09:17 +0100

    Add missing changes
    Signed-off-by: Tomasz Pietrek <tomasz@synadia.com>

--- src/components/NatsFlow/nodes/index.ts
+++ src/components/NatsFlow/nodes/index.ts
@@ -2,3 +2,4 @@ export { BaseNode } from './BaseNode';
 export { PublisherNode } from './PublisherNode';
 export { SubscriberNode } from './SubscriberNode';
 export { ServiceNode } from './ServiceNode';
+export { ServerNode } from './ServerNode';
