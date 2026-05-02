5b2e591e068224101da395c7b76f3f30d1d387f4
scottf <scottfauerbach@gmail.com>
2026-05-02 09:59:16 -0400

    clean deprecated
    Signed-off-by: scottf <scottfauerbach@gmail.com>

--- static/examples/snippets/cli/pubsub/logging.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Application components publish logs
-nats pub logs.error "Database connection failed"
-nats pub logs.info "Service started successfully"
-nats pub logs.debug "Processing request ID: 12345"
-
-# Centralized logger subscribes to all log levels
-nats sub 'logs.>'
--- static/examples/snippets/cli/pubsub/wildcard-single.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Subscribes to all US weather updates
-nats sub 'weather.us.*'
-
-# Matches:
-# ✓ weather.us.california
-# ✓ weather.us.newyork
-# ✗ weather.us.california.sandiego (too many tokens)
